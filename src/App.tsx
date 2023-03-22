9b2c459fbbca464e405fbc7e8745e7a787bfc1b0
nimmala-srinivasu <srinivasu4321@gmail.com>
2023-03-22 13:20:14 +0530

    commit02-added Alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import Button from './components/Button'
+import Alert from './components/Alert'
+import { useState } from 'react'
+
 
 function App() {
+  const [alertVisible, setAlertVisible] = useState(false)
 
   return (
     <div className="App">
-      <Button color='danger' onClick={()=>console.log("clicked")}>Start</Button>
+      {alertVisible && <Alert onClose={()=>setAlertVisible(false)}>warning</Alert>}
+      <Button color='danger' onClick={()=>setAlertVisible(true)}>Start</Button>
     </div>
   )
 }
